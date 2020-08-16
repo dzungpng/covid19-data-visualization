d67a3a89d477714652589a3cedc2258aa63bb260
Dzung Nguyen <dzungng@seas.upenn.edu>
2020-08-16 00:43:53 -0400

    Updates

---
+++ precache-manifest.e77d88a66b9d24e5fb743cf99bcee3ef.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f7ebaff45e7087cdd9daad018359ec6",
+    "url": "/covid19-data-visualization/index.html"
+  },
+  {
+    "revision": "410f0c4c76b0218157de",
+    "url": "/covid19-data-visualization/static/css/2.e1625d90.chunk.css"
+  },
+  {
+    "revision": "e7cc3a2fcb3f6048d1a1",
+    "url": "/covid19-data-visualization/static/css/main.b9c2bc09.chunk.css"
+  },
+  {
+    "revision": "410f0c4c76b0218157de",
+    "url": "/covid19-data-visualization/static/js/2.f7431fad.chunk.js"
+  },
+  {
+    "revision": "d3eab2c800484648f446253871549a7a",
+    "url": "/covid19-data-visualization/static/js/2.f7431fad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7cc3a2fcb3f6048d1a1",
+    "url": "/covid19-data-visualization/static/js/main.22369b98.chunk.js"
+  },
+  {
+    "revision": "9c163c9a0535fa39d8bd",
+    "url": "/covid19-data-visualization/static/js/runtime-main.40243c11.js"
+  },
+  {
+    "revision": "e4fb7ce9983b0dbcddb32197ae0350d8",
+    "url": "/covid19-data-visualization/static/media/globe2.e4fb7ce9.png"
+  },
+  {
+    "revision": "6f3bf9a1b6b834ffb7d1972a815a9217",
+    "url": "/covid19-data-visualization/static/media/small_data_deaths.6f3bf9a1.czml"
+  },
+  {
+    "revision": "47a78e2848b3de8dde28e5659c54953e",
+    "url": "/covid19-data-visualization/static/media/small_data_deaths_pm.47a78e28.czml"
+  },
+  {
+    "revision": "089871b84dadfa688b6c1746c373a5f4",
+    "url": "/covid19-data-visualization/static/media/small_data_total.089871b8.czml"
+  },
+  {
+    "revision": "b443d2fdc3b84f76e956795a5d48e935",
+    "url": "/covid19-data-visualization/static/media/small_data_total_pm.b443d2fd.czml"
+  },
+  {
+    "revision": "94c01f5d91af41476621fbf8e78c7350",
+    "url": "/covid19-data-visualization/static/media/subtitle2.94c01f5d.gif"
+  }
+]);
